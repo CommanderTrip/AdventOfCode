b93ebab40b1935666369b4e1182b7628805998f6
Jason <jason.carnahan99@gmail.com>
2022-12-20 10:10:23 -0600

    Added Day 9

---
+++ AoC-22-Kotlin/src/main/kotlin/day09/solution.kt
@@ -0,0 +1,30 @@
+package day09
+
+import java.io.File
+
+
+/*
+Day 9: Rope Bridge
+
+How many positions does the tail of the rope visit at least once?
+
+Part 1
+Sample Solution: 13
+
+Part 2
+Sample Solution:
+
+ */
+fun solution() {
+    // Get the path to txt
+    val pathToInput = Constants.getPath(8, true)
+
+
+    // Build the grid and store it in memory
+    File(pathToInput).forEachLine { line ->
+
+    }
+
+
+}
+}
